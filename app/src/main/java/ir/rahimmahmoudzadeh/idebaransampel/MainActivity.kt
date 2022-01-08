5ba5d28bbffe6a20688837e7328c75128f1c74f5
Rahim_mahmudzade <rahim62146@gmail.com>
2022-01-08 09:03:57 +0330

    Rename function

--- app/src/main/java/ir/rahimmahmoudzadeh/idebaransampel/MainActivity.kt
+++ app/src/main/java/ir/rahimmahmoudzadeh/idebaransampel/MainActivity.kt
@@ -3,12 +3,9 @@ package ir.rahimmahmoudzadeh.idebaransampel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -17,16 +14,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import ir.rahimmahmoudzadeh.idebaransampel.ui.theme.IdeBaranSampelTheme
-import ir.rahimmahmoudzadeh.idebaransampel.ui.theme.SurFace
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -88,7 +82,7 @@ fun ShowTapImage() {
         Column(modifier = Modifier
             .weight(1.3f)
             .padding(2.dp)) {
-            ThreeCardTop(
+            ThreeCard(
                 intArrayOf(
                     R.drawable.car,
                     R.string.missionUser,
@@ -109,7 +103,7 @@ fun ShowTapImage() {
         Column(modifier = Modifier
             .weight(1.3f)
             .padding(2.dp)) {
-            ThreeCardTop(
+            ThreeCard(
                 intArrayOf(
                     R.drawable.car,
                     R.string.add_mission,
@@ -153,7 +147,7 @@ fun SingleCard(name: Int, image: Int) {
 }
 
 @Composable
-fun ThreeCardTop(name: IntArray) {
+fun ThreeCard(name: IntArray) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceAround,
